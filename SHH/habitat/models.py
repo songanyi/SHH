86446cdce4a35edd64e66f6a2b2508e71221a72d
Mengdi Wang <wangmengdi@gmail.com>
2017-06-13 21:52:36 -0700

    fix bugs in model and add migrations

--- SHH/habitat/models.py
+++ SHH/habitat/models.py
@@ -4,6 +4,21 @@ from django.db import models
 from taggit.managers import TaggableManager
 
 '''
+    Contacts
+'''
+class Contact(models.Model):
+    line1 = models.CharField(max_length=150)
+    line2 = models.CharField(max_length=150)
+    postalcode = models.CharField(max_length=10)
+    city = models.CharField(max_length=150)
+    country = models.CharField(max_length=150)
+    email = models.EmailField()
+    phone = models.CharField(max_length=18)
+
+    def __str__(self):
+        pass
+
+'''
     Basic information of the properties
 '''
 class Property(models.Model):
@@ -33,24 +48,14 @@ class Feature(models.Model):
     Images of the properties
     Reference: https://stackoverflow.com/questions/34006994/how-to-upload-multiple-images-to-a-blog-post-in-django
 '''
+
+def get_image_file(instance, filename):
+    return '/'.join(['content', instance.user.username, filename])
+
+
 class Images(models.Model):
     prop = models.ForeignKey(Property, default=None)
     image = models.ImageField(upload_to=get_image_file, verbose_name='Image')
-
-    def __str__(self):
-        pass
-
-'''
-    Contacts
-'''
-class Contact(models.Model):
-    line1 = models.CharField(max_length=150)
-    line2 = models.CharField(max_length=150)
-    postalcode = models.CharField(max_length=10)
-    city = models.CharField(max_length=150)
-    country = models.CharField(max_length=150)
-    email = models.EmailField()
-    phone = models.CharField(max_length=18)
 
     def __str__(self):
         pass
